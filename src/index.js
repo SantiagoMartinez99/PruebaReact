fee51531846d5e23d4e5d58e9ccdb7efdc529f4c
David Martinez <david.martinez@exsis.loc>
2022-02-07 17:22:08 -0500

    primera version pagina react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// const element =document.createElement("h1")
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// element.innerText="Hola React"
+
+// const container=document.getElementById("root")
+
+// container.appendChild(element)
+
+import react from "react";
+import reactDom from "react-dom";
+
+// const element= <h1>Hello React </h1>
+// const container= document.getElementById("root")
+
+// reactDom.render(____QUE,__DONDE)
+
+// reactDom.render(element,container)
+
+
+
+
+const user={
+  firstName: "Raul",
+  lastName: "Palacios",
+  avatar:"https://th.bing.com/th/id/R.bcca3fbdff9aa4b16eebbe5183a79494?rik=CkH5HhmTlQg57Q&pid=ImgRaw&r=0"
+}
+
+function getName(user){
+  return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+  if(user){
+    return <h1>Hola,{getName(user)}</h1>
+  }
+    return <h1>Hola,extraño</h1>
+}
+
+
+const name = "Josh Perez";
+const element =( <div>
+    <h1>{getGreeting(user)}</h1>
+    <img src={user.avatar} alt="" />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+reactDom.render(
+  element,
+  document.getElementById("root")
+);
